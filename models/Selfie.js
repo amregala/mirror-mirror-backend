31229f9522947f605ea0dfdf8993c7f3df3f850b
amregalado <regalado.adriana@gmail.com>
2022-11-05 22:11:28 -0400

    Selfie schema updated, properties added

--- models/Selfie.js
+++ models/Selfie.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const selfieSchema = new schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  uploadDate: { type: String, default: Date.now },
-  imgMedium: { type: String, default: "unknown" },
-  likes: { type: String, default: 0 },
-  tags: [{ type: String }],
-  creditLine: { type: String }
-  //   year: { type: String, required: true },
-});
+const selfieSchema = new schema(
+  {
+    // name: { type: String },
+    selfieArtworkTitle: { type: String },
+    artistCreator: { type: String, required: true },
+    image: { type: String, required: true },
+    yearCreated: { type: String, required: true },
+    //   uploadDate: { timestamps: true },
+    imgMedium: { type: String, default: "unknown" },
+    artistNationality: { type: String },
+    located: { type: String },
+    creditLine: { type: String },
+    likes: { type: String, default: 0 },
+    tags: [{ type: String }],
+  },
+  { timestamps: true }
+);
 
 const Selfie = mongoose.model("Selfie", selfieSchema);
 
